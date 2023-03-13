e645b1b6f601e2750e6ba329c6d9d684c1cfc1be
mytrofanov <mytrofanovmaxym@gmail.com>
2023-03-13 18:18:17 +0200

    feat: roles api

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateRoleDTO } from "./dto/create-role.dto";
+import { InjectModel } from "@nestjs/sequelize";
+import { Role } from "./roles.model";
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+
+  constructor(@InjectModel(Role) private roleRepository: typeof Role) {
+  }
+
+  async createRole(dto: CreateRoleDTO) {
+    return await this.roleRepository.create(dto);
+  }
+
+  async getRoleByValue(value: string) {
+    return await this.roleRepository.findOne({ where: {value}})
+  }
+
+}
