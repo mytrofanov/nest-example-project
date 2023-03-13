35aa6eab0807eb33882aca8359ef58f8cbd0f457
mytrofanov <mytrofanovmaxym@gmail.com>
2023-03-13 11:17:44 +0200

    feat: AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller('/api')
 export class AppController {
+
+  constructor(private appService: AppService) {
+  }
+
   @Get('/users')
   getUsers (){
-    return [{ id:1, name: 'Max' }];
+      return this.appService.getUsers();
   }
 }
