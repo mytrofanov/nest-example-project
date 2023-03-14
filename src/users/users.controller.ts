db0ba12ca70e7763e4aa2ac592341f37e1d2459a
mytrofanov <mytrofanovmaxym@gmail.com>
2023-03-14 10:33:13 +0200

    feat: role guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,8 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { User } from "./users.model";
-import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { Roles } from "../auth/roles-auth.decorator";
+import { RolesGuard } from "../auth/roles.guard";
 
 @ApiTags('Users API')
 @Controller('users')
@@ -20,7 +21,8 @@ export class UsersController {
   }
   @ApiOperation({ summary: 'Getting all users' })
   @ApiResponse({ status: 200, type: [User] })
-  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
   @Get()
   getAll(){
     return this.usersService.getAllUsers();
